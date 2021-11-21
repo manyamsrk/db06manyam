2b4b35e273f4c61f2407e8ad43ee6f5bbda7f8e6
Siva Rama Krishna Manyam <S544906@nwmissouri.edu>
2021-11-21 14:18:46 -0600

    login

--- routes/gas.js
+++ routes/gas.js
@@ -1,6 +1,13 @@
 var express = require('express');
 const gas_controlers= require('../controllers/gas');
 var router = express.Router();
+const secured = (req, res, next) => {
+  if (req.user){
+    return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect("/login");
+}
 /* GET gas */
 router.get('/', gas_controlers.gas_view_all_Page );
 /* GET detail gas page */
@@ -8,7 +15,9 @@ router.get('/detail', gas_controlers.gas_view_one_Page);
 /* GET create gas page */
 router.get('/create', gas_controlers.gas_create_Page);
 /* GET create update page */
-router.get('/update', gas_controlers.gas_update_Page);
+// router.get('/update', gas_controlers.gas_update_Page);
+/* GET update gas page */
+router.get('/update', secured, gas_controlers.gas_update_Page);
 /* GET create gas page */
 router.get('/delete', gas_controlers.gas_delete_Page);
 
